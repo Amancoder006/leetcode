939c237a02a22d7c84d863ac944ef2ec878eb3eb
Amancoder006 <106905153+Amancoder006@users.noreply.github.com>
2023-08-30 21:26:55 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -10,28 +10,39 @@ class Solution
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        vector<int>vis(V,0);
+        int vis[V] = {0};
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>> , greater<pair<int,int>>> pq;
         
         pq.push({0,0});
-        int ans=0;
+        
+        int ans = 0;
+        
+        //  for(auto i:adj[0]){
+        //     for(auto j:i)cout<<j<<" ";cout<<endl;
+        //         // if(vis[i[1]]==0){
+        //         //     ans+=i[2];
+        //         //     pq.push({i[2],i[1]});
+        //         // }
+        //     }
         
         while(!pq.empty()){
             auto it = pq.top();
             pq.pop();
-            
-            int dis= it.first;
+            int dis = it.first;
             int node = it.second;
-            // cout<<dis<<endl;
+            // cout<<dis<<" "<<node<<endl;
             if(vis[node]==0){
-                vis[node]=1;
+                vis[node]++;
                 ans+=dis;
                 for(auto i:adj[node]){
                     // for(auto j:i)cout<<j<<" ";cout<<endl;
-                    pq.push({i[1],i[0]});
+                    if(vis[i[0]]==0){
+                       
+                        pq.push({i[1],i[0]});
+                    }
                 }
             }
-            
         }
         return ans;
     }
